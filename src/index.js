6d12a75de966565160738cc8b15cdda02efa5b02
Priyanshu Sahani <priyanshusahani1119@gmail.com>
2023-06-05 01:07:05 +0530

    rendered books by iterating an array

--- src/index.js
+++ src/index.js
@@ -8,30 +8,35 @@ import './css/index.css';
 //     return <h2>Hello World</h2>;
 // };
 
-const book1 = {
-    img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-    title: 'Atomic Habits',
-    author: 'James Clear',
-};
+const bookData = [
+    {
+        id: 1,
+        img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+        title: 'Atomic Habits',
+        author: 'James Clear',
+    },
+    {
+        id: 2,
+        img: 'https://m.media-amazon.com/images/I/51k+lXZyJ6L._SX322_BO1,204,203,200_.jpg',
+        title: 'Gentleman in Moscow, A',
+        author: 'Amor Towles',
+    },
+    {
+        id: 3,
+        img: 'https://m.media-amazon.com/images/I/51HiMkA09VL._SX324_BO1,204,203,200_.jpg',
+        title: 'All the Light we Cannot See',
+        author: 'Anthony Doerr',
+    },
+];
 
-const book2 = {
-    img: 'https://m.media-amazon.com/images/I/51k+lXZyJ6L._SX322_BO1,204,203,200_.jpg',
-    title: 'Gentleman in Moscow, A',
-    author: 'Amor Towles',
-};
-
-const book3 = {
-    img: 'https://m.media-amazon.com/images/I/51HiMkA09VL._SX324_BO1,204,203,200_.jpg',
-    title: 'All the Light we Cannot See',
-    author: 'Anthony Doerr',
-};
+const bookListElement = bookData.map((book) => {
+    return <Book data={book} key={book.id} />;
+});
 
 const BookList = () => {
     return (
         <section className="book-list">
-            <Book data={book1} />
-            <Book data={book2} />
-            <Book data={book3} />
+            {bookListElement}
             {/* <Book /> */}
         </section>
     );
