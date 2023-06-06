570bcdf90569749fd51bed6c0ed1143ed5cdae9a
Priyanshu Sahani <priyanshusahani1119@gmail.com>
2023-06-06 19:11:18 +0530

    Added title and book ranks

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Book = ({ data: { img, title, author } }) => {
+const Book = ({ data: { id, img, title, author } }) => {
     return (
-        <article className="book">
+        <li className="book">
+            <div className="rank">#{id}</div>
             <img src={img} alt={title} />
             <h2>{title}</h2>
             <h4>{author}</h4>
-        </article>
+        </li>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,46 +4,26 @@ import ReactDOM from 'react-dom/client';
 import Book from './Book';
 
 import './css/index.css';
-import ExEvent from './ExEvent';
+import books from './books';
 // const Greeting = () => {
 //     return <h2>Hello World</h2>;
 // };
 
-const bookData = [
-    {
-        id: 1,
-        img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-        title: 'Atomic Habits',
-        author: 'James Clear',
-    },
-    {
-        id: 2,
-        img: 'https://m.media-amazon.com/images/I/51k+lXZyJ6L._SX322_BO1,204,203,200_.jpg',
-        title: 'Gentleman in Moscow, A',
-        author: 'Amor Towles',
-    },
-    {
-        id: 3,
-        img: 'https://m.media-amazon.com/images/I/51HiMkA09VL._SX324_BO1,204,203,200_.jpg',
-        title: 'All the Light we Cannot See',
-        author: 'Anthony Doerr',
-    },
-];
-
-const bookListElement = bookData.map((book) => {
+const bookListElement = books.map((book) => {
     return <Book data={book} key={book.id} />;
 });
 
 const BookList = () => {
     return (
-        <section className="book-list">
-            {bookListElement}
-            {/* <Book /> */}
-        </section>
+        <article className="bestsellars">
+            <h1>BestSellars</h1>
+
+            <ul className="book-list">{bookListElement}</ul>
+        </article>
     );
 };
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 // root.render(<Greeting />);
 root.render(<BookList />);
-root.render(<ExEvent />);
+// root.render(<ExEvent />);
